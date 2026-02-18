86307a4a93b61e940bb286e19ba697b944c2bc84
Jānis Džeriņš <janis.dzerins@cert.lv>
2026-02-18 14:00:58 +0200

    Adjust paths after file renames

--- testing/Scripts/standalone/test.sh
+++ testing/Scripts/standalone/test.sh
@@ -3,7 +3,7 @@
 set -eu
 
 BASEDIR="$(dirname $0)"
-ANALYZER="${BASEDIR}/../../analyzer/iec104.spicy"
+ANALYZER="${BASEDIR}/../../../analyzer/iec104.spicy"
 
 [ ! -f /tmp/iec104 -o "$ANALYZER" -nt /tmp/iec104 ] && spicy-build -d -o /tmp/iec104 "$ANALYZER"
 
